5f6bd10a0ad792564d5a6f5ac5b55d44fcc844f0
Fabio Berta <fnberta@gmail.com>
2019-11-01 21:57:21 +0100

    update location

--- src/sections/LocationMap.tsx
+++ src/sections/LocationMap.tsx
@@ -9,7 +9,7 @@ const IFrame = styled.iframe({
 const LocationMap: React.FC = () => (
   <section className="section is-paddingless">
     <IFrame
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2695.490206617615!2d8.722059115599032!3d47.49984380333973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479a997774549033%3A0x237a9866e8a0e98!2sStadthausstrasse+24%2C+8400+Winterthur!5e0!3m2!1sde!2sch!4v1546035117242"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6322.961483916522!2d8.720848933510352!3d47.497922107394075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479a9973b8957f2f%3A0xb4e1a93adfcddfcd!2sLagerpl.%206%2C%208400%20Winterthur!5e0!3m2!1sde!2sch!4v1572641793515!5m2!1sde!2sch"
       title="Location Map"
       width="100%"
       height="100%"
