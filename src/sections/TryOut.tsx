e55e34020e4c99b40d66797885003db636c1c94b
Fabio Berta <fnberta@gmail.com>
2020-03-08 21:42:46 +0100

    more room to breath

--- src/sections/TryOut.tsx
+++ src/sections/TryOut.tsx
@@ -7,14 +7,14 @@ const TryOut: React.FC = () => (
     style={{ backgroundImage: `url(${require('../images/warrior.jpg')})` }}
   >
     <div className="relative max-w-screen-xl mx-auto px-8 py-20">
-      <div className="-mt-12 -ml-12 flex flex-wrap items-center font-bold">
-        <div className="w-3/5 flex-auto mt-12 ml-12 font-bold">
-          <p className="text-4xl leading-tight">
+      <div className="-mt-12 -ml-12 flex flex-wrap items-center">
+        <div className="w-3/5 flex-auto mt-12 ml-12">
+          <p className="text-4xl leading-snug font-semibold">
             <span className="text-white">Let's do this!</span>
             <br />
             <span className="text-orange-500">Starte heute mit einem Probetraining.</span>
           </p>
-          <p className="mt-2 text-white text-2xl leading-tight">
+          <p className="mt-4 text-white text-2xl leading-tight">
             Für dein Wohlbefinden – für deinen Erfolg – für dich!
           </p>
         </div>
