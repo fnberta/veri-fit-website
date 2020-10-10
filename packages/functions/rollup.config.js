b3bc6ad851d291947ba400f2da8ecd53e98772d5
Fabio Berta <fnberta@gmail.com>
2020-10-10 20:17:19 +0200

    simpler build setup

--- packages/functions/rollup.config.js
+++ packages/functions/rollup.config.js
@@ -1,12 +1,11 @@
-import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
 export default {
-  input: 'src/index.ts',
+  input: 'build/index.js',
   output: {
     format: 'cjs',
     file: pkg.main,
   },
-  plugins: [resolve({ resolveOnly: [/^@veri-fit\/.*$/] }), typescript()],
+  plugins: [resolve({ resolveOnly: [/^@veri-fit\/.*$/] })],
 };
