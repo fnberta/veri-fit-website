2206fac3023aba08cade36db60bd9f3029d5ee78
Fabio Berta <fnberta@gmail.com>
2020-10-10 15:24:37 +0200

    make sure font is loaded

--- packages/landing/src/common/DocHead.tsx
+++ packages/landing/src/common/DocHead.tsx
@@ -78,7 +78,6 @@ const DocHead: React.FC<Props> = ({ title, lang = 'de-CH', meta = [], keywords =
     <Helmet titleTemplate={`%s | ${siteMetadata.title}`} meta={getMeta(meta, keywords, siteMetadata)}>
       <html lang={lang} />
       <title>{title}</title>
-      <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
       <link rel="icon" type="image/png" href={require('../images/favicon-32x32.png')} sizes="32x32" />
       <link rel="icon" type="image/png" href={require('../images/favicon-16x16.png')} sizes="16x16" />
     </Helmet>
