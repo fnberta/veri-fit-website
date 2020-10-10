7e712a0c0a487cef78fb90e366b346772f80304b
Fabio Berta <fnberta@gmail.com>
2020-10-10 16:05:34 +0200

    proper jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
-  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  projects: ['<rootDir>/packages/functions', '<rootDir>/packages/manage'],
 };
