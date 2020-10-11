3e848fa29447425b2b473bc16922e07ea137dc4c
Fabio Berta <fnberta@gmail.com>
2020-10-11 14:44:42 +0200

    prevent shrinking in webkit

--- packages/manage/src/clients/Clients.tsx
+++ packages/manage/src/clients/Clients.tsx
@@ -140,7 +140,7 @@ const Clients: React.FC<ClassNameProps> = ({ className }) => {
   return (
     <section className={cx('flex bg-gray-100 min-h-0', className)}>
       <div className="w-full max-w-screen-xl mx-auto py-6 px-4 flex flex-col">
-        <header className={cx(clientId ? 'hidden' : 'flex', 'lg:flex justify-between items-baseline')}>
+        <header className={cx(clientId ? 'hidden' : 'flex', 'flex-shrink-0 lg:flex justify-between items-baseline')}>
           <h1 className="text-2xl font-semibold">Kunden</h1>
           <Button icon="user-add" onClick={() => setAddDialogOpen(true)}>
             Hinzufügen
